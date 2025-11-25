d7cb3a48bb17723d6d2b0bcaeaa0dad6b978cd13
Yoav Cohen <yoavc4@mail.tau.ac.il>
2025-11-25 23:21:56 +0200

    remove comment

--- data_set_utils/data_examples.py
+++ data_set_utils/data_examples.py
@@ -90,7 +90,6 @@ def print_examples(dataset_name, files, speakers_meta, num_examples=2):
 if __name__ == "__main__":
     data_set_name = "test-clean"  # "dev-clean" or "test-clean"
 
-    # EDIT HERE: put your local LibriSpeech path (directory containing dev-clean/, test-clean/, SPEAKERS.TXT)
     LIBRISPEECH_ROOT = fr"J:\My Drive\Courses\2026A\Signal Processing and Machine Learning for Speech\HW\HW1\SpeechLearningCourseEx1\data\{data_set_name}\LibriSpeech"
 
     speakers_file = os.path.join(LIBRISPEECH_ROOT, "SPEAKERS.TXT")
